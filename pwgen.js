1257c4e0142c34d8b58a6aa25d3d31e891f24702
James Pickering <james.pickering@hermes-europe.co.uk>
2014-04-11 10:59:16 +0100

    Modified salt algorithm - prevoius salt algorithm ignored some of site name

--- pwgen.js
+++ pwgen.js
@@ -1,7 +1,7 @@
 angular.module('pwgen', ["ngAnimate",'mgcrea.ngStrap.tooltip', 'mgcrea.ngStrap.popover','mgcrea.ngStrap.typeahead']);
 
 function PwGen($scope) {
-  //$scope.salt = CryptoJS.enc.Hex.parse('0000000000000000'); // Hard-coded - only way to make this repeatable
+  var salt = CryptoJS.enc.Hex.parse('0000000000000000'); // Hard-coded - only way to make this repeatable
   $scope.userName = localStorage.userName || "";
   $scope.siteName = "";
   $scope.pw1 = "";
@@ -21,8 +21,7 @@ function PwGen($scope) {
     if ($scope.pw1 !== $scope.pw2) {
       $scope.password = "Passwords do not match!"
     } else {
-      var salt = CryptoJS.enc.Hex.parse($scope.userName + ":" + $scope.siteName);
-      var key = CryptoJS.PBKDF2($scope.pw1, salt, {keySize: 128/32, iterations: $scope.iterations});
+      var key = CryptoJS.PBKDF2($scope.userName + ":" + $scope.siteName + ":" + $scope.pw1, salt, {keySize: 128/32, iterations: $scope.iterations});
       var b64Key = CryptoJS.enc.Base64.stringify(key)
       $scope.password = b64Key.substring(0, 7) + "." + b64Key.substring(7,13)
       $scope.saveSites()
